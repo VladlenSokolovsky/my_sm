37fc98ec3eb8bed469e32c21dafc7ac59ed08d31
VladlenSokolovsky <vladlensokolovskii@gmail.com>
2022-10-13 15:31:27 +0200

    Separate general and app html, add clickable link for each room, separate local settings + it in gitignore

--- my_SM/settings/main.py
+++ my_SM/settings/main.py
@@ -10,23 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-aqfh@xan-lz09q!6hq4q$6&8@e#2#qu&g+=6bg_ja#7gepqz=g'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+from .local import BASE_DIR
 
 # Application definition
 
@@ -56,7 +43,9 @@ ROOT_URLCONF = 'my_SM.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / '../templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -70,17 +59,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'my_SM.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
