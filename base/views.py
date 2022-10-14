e5987430e1b774af4238bdf222b8cf1e1251ec8d
VladlenSokolovsky <vladlensokolovskii@gmail.com>
2022-10-14 18:20:06 +0200

    User login and slash messages

--- base/views.py
+++ base/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from .models import Room, Topic
 from .forms import RoomForm
+
+
+def login_page(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'Incorrect username')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Incorrect username or password')
+    context = {}
+    return render(request, 'base/login_register.html', context)
 
 
 def home(request):
