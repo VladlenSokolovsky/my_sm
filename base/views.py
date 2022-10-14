6c27f3cc90b32a39eab4de9cff2b2f6396b64cec
VladlenSokolovsky <vladlensokolovskii@gmail.com>
2022-10-14 13:07:40 +0200

    Make view of all/separate topics and view of updated and deleted room

--- base/views.py
+++ base/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
 
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms': rooms}
+    q = request.GET.get('q') if request.GET.get('q') else ''
+    rooms = Room.objects.filter(topic__name__icontains=q)
+
+    topics = Topic.objects.all()
+
+    context = {'rooms': rooms, 'topics': topics}
     return render(request, 'base/home.html', context)
 
 
@@ -24,3 +28,24 @@ def create_room(request):
             return redirect('home')
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
+
+
+def update_room(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def delete_room(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == "POST":
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj': room})
