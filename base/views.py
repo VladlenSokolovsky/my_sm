cf3aa8716050ccec2e0dac64afe859afb7be11e8
VladlenSokolovsky <vladlensokolovskii@gmail.com>
2022-10-15 11:14:19 +0200

    Add participants, add deleting function for messages(comments)

--- base/views.py
+++ base/views.py
@@ -71,6 +71,7 @@ def home(request):
 def room(request, pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
 
     if request.method == 'POST':
         message = Message.objects.create(
@@ -78,8 +79,9 @@ def room(request, pk):
             room=room,
             body=request.POST.get('body'),
         )
+        room.participants.add(request.user)
         return redirect('room', pk=room.id)
-    context = {'room': room, 'room_messages': room_messages}
+    context = {'room': room, 'room_messages': room_messages, 'participants': participants}
     return render(request, 'base/room.html', context)
 
 
@@ -123,3 +125,16 @@ def delete_room(request, pk):
         room.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj': room})
+
+
+@login_required(login_url='login')
+def delete_message(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse("It is not allowed to delete other people's messages!!!")
+
+    if request.method == "POST":
+        message.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj': message})
